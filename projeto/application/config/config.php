8fdbef177aafa3084441fd68228f8caedb5cdbb6
abnergit <denysabner@gmail.com>
2017-06-01 16:30:06 -0300

    Update config.php

--- projeto/application/config/config.php
+++ projeto/application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost/projeto/';
+$config['base_url'] = 'https://unbook.herokuapp.com/projeto';
 
 /*
 |--------------------------------------------------------------------------
